a901cba60797f9eb061e328a9d50b7c9231ea136
Jonathan Gao <jon.t.gao@gmail.com>
2025-06-09 22:25:32 -0500

    banner enter animation: working on specifying classes for sequencing

--- react-docker/src/app/components/nameplate.tsx
+++ react-docker/src/app/components/nameplate.tsx
@@ -2,7 +2,7 @@
 
 import * as motion from "motion/react-client"
 import { AnimatePresence, AnimationScope, useAnimate } from "motion/react"
-import { useState, useEffect } from "react"
+import { useState, useEffect, forwardRef } from "react"
 
 const subtitles = [
     "ML Engineer",
@@ -14,7 +14,7 @@ const subtitles = [
     "Mario Kart Racer",
 ]
 
-function NameplateStar() {
+const NameplateStar = forwardRef<HTMLDivElement>((props, scope) => {
     return (
         <div className="relative -top-[9em] right-[6em]">
             <motion.img
@@ -25,8 +25,9 @@ function NameplateStar() {
             />
         </div>
     )
-}
-function NameplateSubtitle() {
+})
+
+const NameplateSubtitle = forwardRef<HTMLDivElement>((props, scope) => {
     const [index, setIndex] = useState(0)
     useEffect(() => {
         const interval = setInterval(() => {
@@ -39,38 +40,47 @@ function NameplateSubtitle() {
         <AnimatePresence mode="wait">
             <motion.div
                 key={index}
-                initial={{ y: 20, opacity: 0 }}
+                initial={{ y: 15, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                exit={{ y: -20, opacity: 0 }}
-                transition={{ duration: 0.3 }}
+                exit={{ y: -15, opacity: 0 }}
+                transition={{ duration: 0.2 }}
                 className='text-style-display-subtitle'
             >
                 {subtitles[index]}
             </motion.div>
         </AnimatePresence>
     )
-}
-function NameplateText() {
+})
+
+const NameplateText = forwardRef<HTMLDivElement>((props, scope) => {
     return (
         <div className="w-full h-full flex flex-col justify-end items-center pb-[1.17113rem]">
             <div className="flex flex-col gap-[1.08rem] text-onwhite">
-                <div className='text-style-display-title'>Jonathan Gao</div>
-                <NameplateSubtitle />
+                <div ref={scope} className='text-style-display-title'>Jonathan Gao</div>
+                <NameplateSubtitle ref={scope}/>
             </div>
         </div>
     )
-}
-export default function Nameplate() {
+})
+
+const Nameplate = () => {
     const [scope, animate] = useAnimate()
 
     useEffect(() => {
-        animate("li", { opacity: 1} )
-    })
+        const enterSequence = async () => {
+            await animate("*", { opacity: 0 }, { duration: 0 })
+            await animate(".banner-first", { transform: "scaleX(1)" })
+        }
+        enterSequence()
+    }, [scope])
 
     return (
         <div className="h-[53.625rem] flex justify-center">
             <motion.div
-                className="w-[62.625rem] h-[30.6875rem] mt-[10.5rem] p-[3rem] bg-pitch rounded-[2rem]"
+                ref={scope}
+                className="w-[62.625rem] h-[30.6875rem] mt-[10.5rem] p-[3rem]
+                         bg-pitch rounded-[2rem]
+                           banner-first"
             >
                 <NameplateStar ref={scope}/>
                 <NameplateText ref={scope}/>
@@ -78,3 +88,4 @@ export default function Nameplate() {
         </div>
     )
 }
+export default Nameplate
