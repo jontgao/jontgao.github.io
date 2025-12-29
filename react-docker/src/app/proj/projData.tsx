1ce91e1049579bf54ddf3025659ac9d3cd41f3a0
Jonathan Gao <jon.t.gao@gmail.com>
2025-12-29 14:56:09 -0600

    a href to next link

--- react-docker/src/app/proj/projData.tsx
+++ react-docker/src/app/proj/projData.tsx
@@ -3,6 +3,7 @@ import { LinkButtonRow } from "../components/link_button";
 import TLDR from "../components/tldr"
 import { CaptionedImage, Caption } from "../components/captioned_image";
 import TwoCol from "../components/two_col";
+import Link from 'next/link'
 
 export enum ProjCategories {
   Featured,
@@ -155,7 +156,7 @@ export const projData: Proj[] = [
                         <li><b>Deployment:</b> I developed and integrated a <a href="https://github.com/jontgao/waggle-plugin-solar-irradiance-forecasting">plug-in</a> to deploy the model on edge devices, enabling real-time data collection and on-device inference.</li>
                     </ul>,
                     <p key="anl-4-2">Ultimately, my work at Argonne grew my skills in ML model design, implementation, and deployment in real-world settings. Further, I gained extensive hands-on experience with data pre-processing and handling oh-so-many data quality issues. I am very grateful to have had the opportunity to collaborate with my supervisors in such a high-impact project.</p>,
-                    <p key="anl-4-2">While newer architectures are now leading the field in time-series prediction, my work ar Argonne played a critical role in developing an effective forecasting model for time-dependent data, especially with the constraints of edge deployment. As I have continued to learn about the ever-evolving energy field (like with my <a href="/proj/siu">research at SIU</a>), I hope to create further impact in projects like this.</p>,
+                    <p key="anl-4-2">While newer architectures are now leading the field in time-series prediction, my work ar Argonne played a critical role in developing an effective forecasting model for time-dependent data, especially with the constraints of edge deployment. As I have continued to learn about the ever-evolving energy field (like with my <Link href="/proj/siu">research at SIU</Link>), I hope to create further impact in projects like this.</p>,
                     <p key="anl-4-2">My resulting plug-in and poster for my project can be viewed below.</p>,
                     <LinkButtonRow key="anl-1" linkButtonProps={[{linkUrl:"https://github.com/jontgao/waggle-plugin-solar-irradiance-forecasting", buttonProps:{text: "Project Github"}}]}/>,
                     <object key="anl-2-5" data="/proj/anl/Gao-Jonathan-SULI-MCS.pdf" type="application/pdf" width="100%" height="720" />,
@@ -374,7 +375,7 @@ export const projData: Proj[] = [
                         right={<Caption caption="Adobe Photoshop, Adobe After Effects, Adobe Illutrator. 2020. A profile picture design for my dorm.">
                                 <video controls loop playsInline className="w-full"><source src="/proj/misc-graphic/error404.mp4" type="video/mp4" /> Your browser does not support the video tag.</video>
                             </Caption>}
-                        leftFraction="1/2"
+                        emphasis="none"
                     />,
                     <CaptionedImage key="misc-graphic-0-2" src="/proj/misc-graphic/turkeyrun.png" caption="Adobe Illustrator. 2023. A promotional social media graphic for a club." />,
                     <CaptionedImage key="misc-graphic-0-3" src="/proj/misc-graphic/springfling.jpg" caption='Adobe Photoshop. 2019. An "Avengers Endgame"-styled logo I made for a school dance.' />,
