1aa8d7a04021a547ae8f3f63c14ce879346ed2bb
Jonathan Gao <jon.t.gao@gmail.com>
2026-01-08 11:11:39 -0600

    motion.a -> next.js Link

--- react-docker/src/app/components/card.tsx
+++ react-docker/src/app/components/card.tsx
@@ -1,27 +1,30 @@
 import * as motion from "motion/react-client"
 import { Proj } from "../proj/projData"
+import Link from "next/link"
 
 
 export default function Card({slug, externalLink, thumbnailRef, metaTitle}: Proj) {
   return (
-    <motion.a
-      layout
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0, opacity: 0, transition: { duration: 0.2 } }}
-      whileHover={{
-        scale: 1.03,
-        transition: { duration: 0.1 },
-      }}
-      href={externalLink ? externalLink : `/proj/${(encodeURIComponent(slug))}`}
-      className={`flex flex-col space-y-card-gap`}
-    >
-      <img
-        className="w-full rounded-image aspect-square object-cover"
-        src={thumbnailRef}
-        alt={metaTitle}
-      />
-      <div className="text-style-card-title">{metaTitle}</div>
-    </motion.a>
+    <Link href={externalLink ? externalLink : `/proj/${(encodeURIComponent(slug))}`}>
+      <motion.div
+        layout
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0, opacity: 0, transition: { duration: 0.2 } }}
+        whileHover={{
+          scale: 1.03,
+          transition: { duration: 0.1 },
+        }}
+        // href={externalLink ? externalLink : `/proj/${(encodeURIComponent(slug))}`}
+        className={`flex flex-col space-y-card-gap`}
+      >
+        <img
+          className="w-full rounded-image aspect-square object-cover"
+          src={thumbnailRef}
+          alt={metaTitle}
+        />
+        <div className="text-style-card-title">{metaTitle}</div>
+      </motion.div>
+    </Link>
   )
 }
--- react-docker/src/app/components/link_button.tsx
+++ react-docker/src/app/components/link_button.tsx
@@ -1,6 +1,7 @@
 'use client'
 import * as motion from "motion/react-client"
 import {SolidButton, ButtonProps} from "./button"
+import Link from "next/link"
 
 export interface LinkButtonProps {
     linkUrl: string
@@ -8,15 +9,16 @@ export interface LinkButtonProps {
 }
 export function LinkButton({linkUrl, buttonProps}: LinkButtonProps) {
     return (
-        <motion.a
+      <Link href={linkUrl}>
+        <motion.div
           whileHover={{
             scale: 1.05,
             transition: { duration: 0.1 },
           }}
-          href={linkUrl}
         >
             <SolidButton {...buttonProps}/>
-        </motion.a>
+        </motion.div>
+      </Link>
     )
 }
 interface LinkButtonRowProps {
